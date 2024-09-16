4ccc490247f4a03a48f8f7d90ecccc76321875d4
Danilo Korać <72507305+danilokorac@users.noreply.github.com>
2024-09-16 16:42:32 +0200

    State, Events, and Forms Interactive Components - PART 1 Done

---
+++ steps/src/App.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState();
+
+  function handlePrevious() {
+    step > 1
+      ? setStep((step) => step - 1)
+      : alert("There are no previous steps");
+    console.log(step);
+  }
+
+  function handleNext() {
+    // when updating the state based on the current state value use (state) => / do something with it /
+    step < 3 ? setStep((step) => step + 1) : alert("There are no more steps");
+    console.log(step);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step === 1 ? "active" : ""}`}>1</div>
+            <div className={`${step === 2 ? "active" : ""}`}>2</div>
+            <div className={`${step === 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
